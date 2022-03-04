836a3afac646b8f77d6e6f5c58ec096b92eca927
Mathew Sida <Sida.mathew@gmail.com>
2022-03-04 17:31:26 +0000

    pic size

--- index.php
+++ index.php
@@ -1,21 +1,45 @@
 <html>
-    <head>
-        <title>PHP File!</title>
-    </head>
-<body>
-    <h1>PHP File!</h1>
+<head>
+<title>Bond Web Service Demo</title>
+<style>
+body {font-family:georgia;}
 
-    <p>This is HTML!</p>
+  .film{
+    border:1px solid #E77DC2;
+    border-radius: 5px;
+    padding: 5px;
+    margin-bottom:5px;
+    position:relative;   
+  }
+ 
+  .pic{
+    position:absolute;
+    right:10px;
+    top:10px;
+  }
 
-    <?php
 
-    echo "<p>This is PHP!</p>";
+   </style>
+<script src="https://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
 
-    ?>
+<script type="text/javascript">
 
-    <p>This is HTML!</p>
+function bondTemplate(film){
+  return `<div class="film">
+  <b>Film: </b> ${film.Film}<br />
+  <b>Title: </b> ${film.Title}<br />
+  <b>Year: </b> ${film.Year}<br />
+  <b>Director: </b> ${film.Director}<br />
+  <b>Producers: </b> ${film.Producers}<br />
+  <b>Writers: </b> ${film.Writers}<br />
+  <b>Composer: </b> ${film.Composer}<br />
+  <b>Bond: </b> ${film.Bond}<br />
+  <b>Budget: </b> ${film.Budget}<br />
+  <b>BoxOffice: </b> ${film.BoxOffice}<br />
+  <div class="pic"><img src="thumbnails/${film.Image}" /></div>
+</div>`;
+}
 
-    <p>Hello from repl.it!</p>
 </body>
 
 </html>
